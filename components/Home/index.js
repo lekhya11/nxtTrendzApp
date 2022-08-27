076f6dc0ed597fb258151ff0e3c71682ce327ea6
k.lekhya <107404763+lekhya11@users.noreply.github.com>
2022-08-27 12:46:14 +0530

    Update index.js

--- components/Home/index.js
+++ components/Home/index.js
@@ -1,20 +1,19 @@
-import Header from '../Header'
+import {Link} from 'react-router-dom'
 
+import Header from '../Header'
 import './index.css'
 
 const Home = () => (
   <>
     <Header />
-    <div className="app-container">
+    <div className="home-container">
       <div className="home-content">
-        <h1 className="home-heading">Clothes That Get You Noticed</h1>
-
+        <h1 className="home-heading">Clothes That Get YOU Noticed</h1>
         <img
-          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-img.png "
-          alt=" clothes that get you noticed"
-          className="Home-image-min"
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-img.png"
+          alt="clothes to be noticed"
+          className="home-mobile-img"
         />
-
         <p className="home-description">
           Fashion is part of the daily air and it does not quite help that it
           changes all the time. Clothes have always been a marker of the era and
@@ -22,16 +21,16 @@ const Home = () => (
           that way you are. So, celebrate the seasons new and exciting fashion
           in your own way.
         </p>
-
-        <button type="button" className="shop-now-button">
-          Shop Now
-        </button>
+        <Link to="/products">
+          <button type="button" className="shop-now-button">
+            Shop Now
+          </button>
+        </Link>
       </div>
-
       <img
-        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-img.png "
-        alt=" clothes that get you noticed"
-        className="Home-image-desktop"
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-img.png"
+        alt="dresses to be noticed"
+        className="home-desktop-img"
       />
     </div>
   </>
